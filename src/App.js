d6294aeb6ddd76d6227ae3e1e2320e096e098392
Alex Chin <alexchin1999@outlook.com>
2022-03-27 17:31:52 -0400

    connected backend with frontend to display a simple message

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import Button from './components/Button';
 import Header from './components/Header';
 import Image from './components/Image';
 import Form from './components/Form';
 import Social from './components/Social';
-
-
 
 function App() {
   const [playerSearch, setPlayerSearch] = useState([])
@@ -13,6 +11,26 @@ function App() {
   const showPlayerNames = (newPlayerSearch) => { // function name = showPlayerNames and passes through a parameter newPlayerSearch
     setPlayerSearch(newPlayerSearch)
   }
+
+  const [apiData, setapiData] = React.useState(null);
+
+
+  useEffect(() => {
+    const getApi = async () => {
+      const apiFromServer = await fetchApi()
+      setapiData(apiFromServer)
+    }
+    getApi()
+  }, [])
+
+  const fetchApi = async () => {
+    const res = await fetch('http://localhost:3001/api')
+    const data = await res.json()
+    console.log(data)
+    return data
+  }
+
+  //{!apiData ? "Loading..." : apiData.message}  
 
   return (
     < div className='backgroundPage' >
@@ -22,21 +40,31 @@ function App() {
       </div>
       <Form getPlayerNames={showPlayerNames} />
       <Button />
+
       <div className='landingInfo'>
         <div className='column'>
           <h4> Chart the Climb</h4>
           <span>"Create leaderboards with your friends to visualize the ranked season" </span>
           <div><img src="./images/phone.PNG" alt="" id="image2" /></div>
         </div>
+
         <div className='column'>
           <h4> Track Your Daily Progress</h4>
-          <span> Keep tabs on today's LP changes and win-loss record at a glance. </span>
-          <div> <img src="./images/ranks.PNG" alt="" className="image3" /></div>
+          <span> Keep tabs on today's RR changes and win-loss record at a glance.</span>
+          <div> <img src="./images/ranks.PNG" alt="" className="image3" />
+
+          </div>
         </div>
         <div className="dockBar">
           <Social />
         </div>
       </div>
+
+      <div className='backgroundImage'>
+        <h1> hello</h1>
+      </div>
+
+
     </div>
 
   )
