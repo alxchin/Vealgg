7390aa120bfc97f46541b6f3eb6dfc0c786b3c2e
Alex Chin <alexchin1999@outlook.com>
2022-04-06 00:42:56 -0400

    looped over data and extracted certain values to graph

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,7 +1,7 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import styled from 'styled-components';
-import { Link } from 'react-router-dom';
-import { useNavigate } from 'react-router-dom';
+import { useNavigate, Link } from 'react-router-dom';
+
 
 
 const Input = styled.input`
@@ -23,46 +23,26 @@ const Submitbutton = styled.input`
   border: none;
   border-radius: 5px;
   padding: 1.5em 2em;
+`
 
-`;
+const Form = () => {
+  const [players, setPlayers] = useState('');
+  const [region, setRegion] = useState('NA');
 
-class Form extends Component {
+  const navigate = useNavigate()
 
-  constructor(props) {
-    super(props)
-
-    this.state = {
-      players: "",
-      region: "NA",
-      validSearch: false,
-
-    }
-    this.handleSubmit = this.handleSubmit.bind(this)
+  function fetchPlayerStats(formattedPlayerNames) {
+    return fetch("http://localhost:3001/search", { headers: { 'Content-Type': 'application/json' }, method: 'POST', body: JSON.stringify(formattedPlayerNames) })
+      .then(function (res) {
+        return res.json()
+      })
+      .then(function (data) {
+        return data
+      })
   }
 
-  playerhandler = (event) => {
-    this.setState({
-      players: event.target.value
-    })
-  }
-  regionhandler = (event) => {
-    this.setState({
-      region: event.target.value
-    })
-  }
-
-  handleSubmit = (event) => {
-
-
-    event.preventDefault()
-    console.log(this.state)
-    this.setState({
-      players: "",
-      region: "NA",
-    })
-    let playerNamesArray = this.state.players.split(',')
-    let formattedPlayerNames = []
-
+  function validatePlayerNames(formattedPlayerNames) {
+    let playerNamesArray = players.split(',')
     playerNamesArray.forEach(playerName => {
       if (!playerName.includes('#')) {
         playerNamesArray = []
@@ -70,58 +50,48 @@ class Form extends Component {
       }
       else if (playerName.includes('#')) {
         let checkPlayerName = playerName.split('#')
-        if ((checkPlayerName[0]).trim().length < 3) {
+        if ((checkPlayerName[0]).trim().length < 3 && checkPlayerName[0].trim().length <= 16) {
           playerNamesArray = []
           return alert(`Invalid user(s) found: ${checkPlayerName[0]}. Player name's length must be greater than 3`)
         }
-        else if ((checkPlayerName[1]).trim().length < 3) {
+        else if ((checkPlayerName[1]).trim().length < 3 && checkPlayerName[1].trim().length <= 5) {
           playerNamesArray = []
           return alert(`invalid user(s) found: ${checkPlayerName[1]}. '#' length must be greater than 3`)
         }
-        else if (((checkPlayerName[0].trim().length >= 3) && (checkPlayerName[0].trim().length <= 16)) && ((checkPlayerName[1].trim().length >= 3) && (checkPlayerName[1].trim().length <= 5))) {
-          formattedPlayerNames.push({ name: checkPlayerName[0].trim(), tag: checkPlayerName[1].trim() })
-        }
+        formattedPlayerNames.push({ name: checkPlayerName[0].trim(), tag: checkPlayerName[1].trim() })
       }
     })
-
-
-    fetch("http://localhost:3001/search", { headers: { 'Content-Type': 'application/json' }, method: 'POST', body: JSON.stringify(formattedPlayerNames) })
-      .then(function (res) {
-        return res.json()
-      })
-      .then(data => console.log(data))
-
-    this.setState({
-      validSearch: true
-    })
-    console.log(this.state.validSearch)
+    return formattedPlayerNames
   }
 
-  //SKIPPY#124, JACKET#1234, BOBBY#4321
-
-  render() {
-    return (
-      <form onSubmit={this.handleSubmit}>
-        <div className='landingSearch'>
-          <Input type='text' placeholder='Player 1, Player 2, Player 3...' value={this.state.players} required onChange={this.playerhandler} /> <Submitbutton type='submit' value='Search'></Submitbutton>
-          {this.state.validSearch && <Link to="stats" />}
-        </div>
-        <div className='landingRegion'> Region:
-          <select onChange={this.regionhandler} defaultValue="NA" >
-            <option defaultValue required > North America </option>
-            <option value="EU"> Europe</option>
-            <option value="KR"> South Korea</option>
-            <option value="TR"> Turkey</option>
-            <option value="JP"> Japan</option>
-            <option value="BR"> Brazil</option>
-            <option value="LATAM"> Latam</option>
-            <option value="OCE"> Oceania</option>
-          </select> <br />
-        </div>
-      </form>
-
-    )
+  function handleSubmit(e) {
+    e.preventDefault()
+    let formattedPlayerNames = []
+    validatePlayerNames(formattedPlayerNames)
+    if (formattedPlayerNames.length) {
+      fetchPlayerStats(formattedPlayerNames).then((data) => navigate('/stats', { state: data }))
+    }
   }
+  return (
+    <form onSubmit={handleSubmit}>
+      <div className='landingSearch'>
+        <Input type='text' placeholder='Player 1, Player 2, Player 3...' value={players} required onChange={(e) => setPlayers(e.target.value)} /> <Submitbutton type='submit' value='Search'></Submitbutton>
+      </div>
+      <div className='landingRegion'> Region:
+        <select onChange={(e) => setRegion(e.target.value)} defaultValue="NA" >
+          <option defaultValue required > North America </option>
+          <option value="EU"> Europe</option>
+          <option value="KR"> South Korea</option>
+          <option value="TR"> Turkey</option>
+          <option value="JP"> Japan</option>
+          <option value="BR"> Brazil</option>
+          <option value="LATAM"> Latam</option>
+          <option value="OCE"> Oceania</option>
+        </select> <br />
+      </div>
+    </form>
+
+  )
 }
 
 export default Form
--- src/components/Stats.js
+++ src/components/Stats.js
@@ -1,10 +1,79 @@
-import React from 'react'
+import React from 'react';
+import styled from 'styled-components';
+import { Scatter, Line } from 'react-chartjs-2';
+import { Chart as ChartJS, registerables } from 'chart.js';
+import { useLocation } from 'react-router-dom';
+ChartJS.register(...registerables);
+
+const Graph = styled.div`
+    height: 50%;
+    width: 50%;
+    margin: 0 auto;
+`;
 
 const Stats = () => {
+
+    const { state } = useLocation();
+    console.log(state)
+
+
+    let playersRR = [];
+    for (let i = 0; i < state.players.length; i++) {
+        let playersArray = []
+        for (let j = 0; j < state.players[i].timeSeriesRR.length; j++) {
+            playersArray.push(state.players[i].timeSeriesRR[j]['rr'])
+        }
+        playersRR.push(playersArray)
+    }
+
+    let playersDate = [];
+    for (let i = 0; i < state.players.length; i++) {
+        let dateArray = []
+        for (let j = 0; j < state.players[i].timeSeriesRR.length; j++) {
+            dateArray.push(state.players[i].timeSeriesRR[j]['date'])
+        }
+        playersDate.push(dateArray)
+    }
+    console.log(playersDate)
+
+
+    const labels = [
+        'January',
+        'February',
+        'March',
+        'April',
+
+
+    ];
+
+    const label = ['player1', 'player2', 'player3']
+    const lineColors = ['rgb(255,99,132)', 'rgb(5,19,75)', 'rgb(50,120,22)', 'rgb(82,95,42)', 'rgb(105,100,20)']
+
+    const datasets = playersRR.map((data, index) => {
+        return {
+            label: label[index],
+            borderColor: lineColors[index],
+            backgroundColor: lineColors[index],
+            data: data,
+            height: 300,
+            width: 300,
+            options: { maintainAspectRatio: false },
+        }
+    })
+
+    const data = {
+        labels: labels,
+        datasets,
+    };
+
+
+
     return (
-        <div>
-            <h1> hello </h1>
-        </div>
+        <>
+            <Graph>
+                <Line data={data} />
+            </Graph>
+        </>
     )
 }
 
