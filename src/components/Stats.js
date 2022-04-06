9ba47bf65459d9fb13172e8e9518a687b48c6de9
Alex Chin <alexchin1999@outlook.com>
2022-04-06 18:50:34 -0400

    chart test

--- src/components/Stats.js
+++ src/components/Stats.js
@@ -21,30 +21,15 @@ const Stats = () => {
     for (let i = 0; i < state.players.length; i++) {
         let playersArray = []
         for (let j = 0; j < state.players[i].timeSeriesRR.length; j++) {
-            playersArray.push(state.players[i].timeSeriesRR[j]['rr'])
+            playersArray.push({ y: state.players[i].timeSeriesRR[j]['rr'], x: new Date(state.players[i].timeSeriesRR[j]['date']) })
         }
         playersRR.push(playersArray)
     }
 
-    let playersDate = [];
-    for (let i = 0; i < state.players.length; i++) {
-        let dateArray = []
-        for (let j = 0; j < state.players[i].timeSeriesRR.length; j++) {
-            dateArray.push(state.players[i].timeSeriesRR[j]['date'])
-        }
-        playersDate.push(dateArray)
-    }
-    console.log(playersDate)
+    console.log(playersRR)
 
 
-    const labels = [
-        'January',
-        'February',
-        'March',
-        'April',
 
-
-    ];
 
     const label = ['player1', 'player2', 'player3']
     const lineColors = ['rgb(255,99,132)', 'rgb(5,19,75)', 'rgb(50,120,22)', 'rgb(82,95,42)', 'rgb(105,100,20)']
@@ -57,21 +42,34 @@ const Stats = () => {
             data: data,
             height: 300,
             width: 300,
-            options: { maintainAspectRatio: false },
+            options: {
+                maintainAspectRatio: false,
+            },
+
+            showLine: true,
+            fill: false,
         }
     })
 
     const data = {
-        labels: labels,
         datasets,
     };
+
+    const chartOptions = {
+        scales: {
+            x: {
+                type: 'time',
+            }
+        }
+    }
 
 
 
     return (
         <>
             <Graph>
-                <Line data={data} />
+                <Scatter data={data} options={chartOptions} />
+
             </Graph>
         </>
     )
